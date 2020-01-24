94a64bd4daf58a952a177e57c37171b12a531034
Jayphen <jayphen@hyperlab.se>
2020-01-24 20:39:33 +0100

    Add fragmentTypes

--- routeQuery.js
+++ routeQuery.js
@@ -1,28 +1,39 @@
 import gql from "graphql-tag";
+
+const productFragment = gql`
+  fragment ProductFragment on Product {
+    id
+    articleNumber
+    name
+    subName
+    price {
+      incVat
+    }
+    images {
+      url
+    }
+  }
+`;
+
 export const ROUTE_QUERY = gql`
   query route($path: String!) {
     route(path: $path) {
       object {
+        ... on Product {
+          ...ProductFragment
+        }
         ... on Category {
           id
           name
           products {
             totalResults
             result {
-              id
-              articleNumber
-              name
-              subName
-              price {
-                incVat
-              }
-              images {
-                url
-              }
+              ...ProductFragment
             }
           }
         }
       }
     }
   }
+  ${productFragment}
 `;
